fbb7e92817252015233eadbb742466f223072abb
GaCampioto <pass2016>
2017-02-13 23:19:21 -0200

    Ajustes no fileReader.js

--- util/fileReader.js
+++ util/fileReader.js
@@ -3,7 +3,7 @@ let arquivo = process.argv[2];
 
 fs.readFile(arquivo, function(error, buffer){
     console.log('Arquivo lido');
-    fs.writeFile('Arquivo_copiado.jpg', buffer, function(error){
+    fs.writeFile("Arquivo_copiado.jpg", buffer, function(error){
         console.log('Arquivo copiado');
     });
 });
